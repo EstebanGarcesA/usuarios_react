96b80593c73be8d5db96015165e3a10aa817327e
EstebanGarcesA <esteg254@gmail.com>
2026-07-31 18:37:03 -0500

    (feature) Cambio visual de la pagina de home y la barra de navegación

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routerApp } from "./routes/routerApp";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(routerApp);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ src/routes/routerApp.jsx
@@ -0,0 +1,9 @@
+import Home from "../pages/Home"
+
+
+export let routerApp = [
+    {
+        path: "/",
+        element: <Home/>
+    }
+]
